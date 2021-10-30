189043f02fd4be8389b1a0d704acea1ae31fec36
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-10-30 14:05:37 +0530

    Create group_and_datewise_total_number.sql

---
+++ group_and_datewise_total_number.sql
@@ -0,0 +1,9 @@
+select distinct device.advertising_id as did, DATE(TIMESTAMP_MICROS(cast(`event_timestamp` as int64)), "Asia/Kolkata") as date_ , count(distinct c.value.string_value) as groups_joined
+        from `trellatale.analytics_153549617.events_*`, UNNEST(event_params) a,UNNEST(event_params) b, UNNEST(event_params) c
+        where event_name = 'ITEM_CLICK' and a.key = 'item_name' and a.value.string_value = 'btn_join'
+        and b.key = 'current_page_name' and b.value.string_value in ('trell_chat','group_chat')
+    and c.key = 'group_id' 
+        and _table_suffix between '20211001' and '20211015' group by 1,2
+        order by 2
+        
+      
