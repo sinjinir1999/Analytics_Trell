bcb205dbf257734218d4d9060d23d94c96fa6dc3
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-11-01 10:56:12 +0530

    Delete groups_created_at.sql

--- groups_created_at.sql
+++
@@ -1,8 +0,0 @@
---Created between two dates
-select distinct JSON_EXTRACT_SCALAR(data, '$.user_data.aaid') as did, DATE(createdAt) as date_
-from trellatale.trellDbDump.all_installs
-where length(JSON_EXTRACT_SCALAR(data, '$.user_data.app_version'))<=7  
-and JSON_EXTRACT_SCALAR(data, '$.user_data.app_version') >= '6.1.08'
-and TRIM(JSON_EXTRACT(data, "$.last_attributed_touch_data['~advertising_partner_name']"),'"') in ('Google AdWords', 'Facebook') 
-and DATE(createdAt) between '2021-10-01' and '2021-10-15'
-order by 2 
