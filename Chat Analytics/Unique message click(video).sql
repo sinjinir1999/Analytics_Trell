923ed5e01832008cd68b118f9677a3215dfd5eae
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-11-02 14:10:00 +0530

    Create Unique message click(video).sql

---
+++ Chat Analytics/Unique message click(video).sql
@@ -0,0 +1,8 @@
+---Unique message click(video)
+
+select distinct c.value.string_value as chat_group,count(distinct device.advertising_id) as message
+        from `trellatale.analytics_153549617.events_*`, UNNEST(event_params) a,UNNEST(event_params) b,UNNEST(event_params) c ,UNNEST(event_params) d
+    where event_name = 'ITEM_CLICK' and a.key = 'item_name' and a.value.string_value = 'btn_message'
+    and b.key = 'current_page_name' and b.value.string_value = 'group_chat' and c.key = 'group_name' and d.key = 'trell_message_type' and d.value.string_value = 'video'
+and  _table_suffix >='20211025'
+group by 1 order by 2 desc
