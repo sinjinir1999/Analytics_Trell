86885f816f90c1472bfde296011d8bf4f0fe60d1
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-11-02 14:22:27 +0530

    Create Chat group Impressions(joined).sql

---
+++ Chat Analytics/Chat group Impressions(joined).sql
@@ -0,0 +1,7 @@
+--Chat group Impressions(joined)
+select distinct c.value.string_value as chat_group,count(distinct device.advertising_id) as chat_group_Impressions_joined
+        from `trellatale.analytics_153549617.events_*`, UNNEST(event_params) a,  UNNEST(event_params) b,UNNEST(event_params) c
+    where event_name = 'PAGE_LANDING' and a.key = 'current_page_name' and a.value.string_value = 'group_chat'
+    and b.key = 'group_status' and b.value.int_value = 1 and c.key = 'group_name' 
+and  _table_suffix >='20211025'
+        group by 1 order by 2 desc
