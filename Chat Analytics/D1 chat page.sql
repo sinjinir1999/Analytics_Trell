3609c6380a3f1c9fe032d3144d748b3111198c0b
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-11-08 19:49:36 +0530

    Create D1 chat page.sql

---
+++ Chat Analytics/D1 chat page.sql
@@ -0,0 +1,5 @@
+--d1 chat page
+select distinct device.advertising_id as did
+        from `trellatale.analytics_153549617.events_*`, UNNEST(event_params) a
+    where event_name = 'PAGE_LANDING' and a.key = 'current_page_name' and a.value.string_value = 'trell_chat'
+        and _table_suffix = '20211105'
