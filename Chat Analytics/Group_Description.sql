2e5d96ee2318eacb144799d4191afccdec147ba9
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-11-10 13:40:17 +0530

    Update Group_Description.sql

--- Chat Analytics/Group_Description.sql
+++ Chat Analytics/Group_Description.sql
@@ -1,7 +1,32 @@
-Select count(distinct did) group_description
-from
-(select
-    device.advertising_id as did,
-    from `trellatale.analytics_153549617.events_*`, UNNEST(event_params) a, UNNEST(event_params) b,UNNEST(event_params)c
-    where event_name = 'PAGE_LANDING' and  _TABLE_SUFFIX  ='20211107' AND
-    b.key = 'current_page_name' AND b.value.string_value = 'group_description' and app_info.version >= '6.1.08') as A
+SELECT count(DISTINCT B.did) group_description_imp
+FROM (
+	SELECT TRIM(JSON_EXTRACT(data, "$.user_data['aaid']"), '"') AS did
+		,TRIM(JSON_EXTRACT(data, "$.last_attributed_touch_data['~campaign']"), '"') AS campaign
+		,EXTRACT(DATE FROM createdAt) AS install_date
+		,count(*)
+	FROM `trellatale.trellDbDump.all_installs`
+	WHERE TRIM(JSON_EXTRACT(data, "$.last_attributed_touch_data['~advertising_partner_name']"), '"') = 'Facebook'
+		AND length(JSON_EXTRACT_SCALAR(data, '$.user_data.app_version')) <= 7
+		AND JSON_EXTRACT_SCALAR(data, '$.user_data.app_version') >= '6.1.08'
+	GROUP BY 1
+		,2
+		,3
+	) A -- D0 installs
+LEFT JOIN (
+	
+	SELECT DISTINCT device.advertising_id AS did
+	FROM `trellatale.analytics_153549617.events_*`
+	WHERE event_name = 'user_engagement'
+		AND _table_suffix = '{}'
+	) P ON P.did = A.did
+LEFT JOIN 
+(SELECT device.advertising_id AS did
+	,
+FROM `trellatale.analytics_153549617.events_*`
+	,UNNEST(event_params) a
+	,UNNEST(event_params) b
+	,UNNEST(event_params) c
+WHERE event_name = 'PAGE_LANDING'
+	AND _TABLE_SUFFIX = '{}'
+	AND b.KEY = 'current_page_name'
+	AND b.value.string_value = 'group_description') AS B ON P.did = B.did
